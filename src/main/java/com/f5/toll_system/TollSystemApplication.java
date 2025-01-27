bc13ddde655ae88300ada10a4217b91946bfb0ee
evymari <144184061+evymari@users.noreply.github.com>
2025-01-27 09:22:33 +0100

    frist commit

---
+++ src/main/java/com/f5/toll_system/TollSystemApplication.java
@@ -0,0 +1,13 @@
+package com.f5.toll_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f5/toll_system/TollSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.toll_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
